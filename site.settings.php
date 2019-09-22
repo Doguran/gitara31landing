7e6f3488bc7b58db6c5cba6a7f2b1b0f611c7aab
Doguran <doguran@yandex.ru>
2019-09-22 17:50:24 +0400

    22.09.2019

---
+++ site.settings.php
@@ -0,0 +1,19 @@
+<?php
+ return array(
+   "internal_type" => "settings",
+   "background_base_color" => "#ffffff",
+   "text_base_color" => "#003e65",
+   "text_secondary_color" => "#dbf1ff",
+   "header_gradient-start" => "#2986c7",
+   "header_gradient-end" => "#003e65",
+   "newsletter_base_color" => "#004c7c",
+   "form_email" => "support@shapingrain.com",
+   "featured_pricing_badge" => "badge_bestvalue.png",
+   "featured_pricing_badge_selected" => "2",
+   "pricing_background" => "#f0f0f0",
+   "pricing_background_price_inactive" => "#666666",
+   "pricing_background_price_active" => "#464646",
+   "featured_pricing_background_price_inactive" => "#125e90",
+   "featured_pricing_background_price_active" => "#00406a",
+);
+?>
